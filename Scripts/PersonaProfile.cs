5a89ef8ca437e8665fe8ec976297012ed3f9705d
Pedro Xudre <xudre@me.com>
2017-09-27 10:51:33 -0300

    Revisão de instâncias compartilhadas

--- Scripts/PersonaProfile.cs
+++ Scripts/PersonaProfile.cs
@@ -2,56 +2,61 @@ using UnityEngine;
 
 namespace ThePipeCat.Persona
 {
-  /// <summary>
-  /// Persona profile.
-  /// </summary>
-  [CreateAssetMenuAttribute(fileName = "New Persona Profile", menuName = "Persona/Profile")]
-  public class PersonaProfile : ScriptableObject
-  {
-    [SerializeField]
-    private PersonaType m_Type;
-    [SerializeField]
-    private string m_ProfileName = "Profile Name";
-    [SerializeField]
-    private PersonaStats m_Stats;
-    [SerializeField]
-    private PersonaInventory m_Inventory;
-
-    public PersonaType Type
+    /// <summary>
+    /// Persona profile.
+    /// </summary>
+    [CreateAssetMenuAttribute(fileName = "New Persona Profile", menuName = "Persona/Profile")]
+    public class PersonaProfile : ScriptableObject
     {
-      get { return m_Type; }
+        [SerializeField]
+        private PersonaType m_Type;
+        [SerializeField]
+        private string m_Name = "Profile Name";
+        [SerializeField]
+        private PersonaStats m_Stats;
+        [SerializeField]
+        private PersonaInventory m_InventoryProfile; // Shared
+        private PersonaInventory m_Inventory;
+
+        public PersonaType Type
+        {
+            get { return m_Type; }
+        }
+
+        public string Name
+        {
+            get { return m_Name; }
+        }
+
+        public PersonaStats Stats
+        {
+            get { return m_Stats; }
+        }
+
+        public PersonaInventory Inventory
+        {
+            get { return m_Inventory; }
+        }
+
+        // -------------------------------------------------------------------------
+        // Unity Lifecycle:
+        // -------------------------------------------------------------------------
+
+        private void Awake()
+        {
+            // Copy inventory to the memory:
+            if (m_InventoryProfile != null)
+                m_Inventory = Instantiate<PersonaInventory>(m_InventoryProfile);
+        }
+
+        private void OnEnable()
+        {
+
+        }
+
+        private void OnDisable()
+        {
+
+        }
     }
-
-    public string Name
-    {
-      get { return m_ProfileName; }
-    }
-
-    public PersonaStats Stats {
-      get { return m_Stats; }
-    }
-
-    public PersonaInventory Inventory {
-      get { return m_Inventory; }
-    }
-
-    // -------------------------------------------------------------------------
-    // Unity Lifecycle:
-    // -------------------------------------------------------------------------
-
-    private void Awake()
-    {
-
-    }
-
-    private void OnEnable()
-    {
-
-    }
-
-    private void OnDisable()
-    {
-
-    }
-  }
 }
--- Scripts/PersonaStats.cs
+++ Scripts/PersonaStats.cs
@@ -2,27 +2,41 @@ using UnityEngine;
 
 namespace ThePipeCat.Persona
 {
-  [System.Serializable]
-  public class PersonaStats : System.Object {
-    [SerializeField]
-    protected float m_Life;
-    [SerializeField]
-    protected float m_LifeMax;
-    [SerializeField]
-    protected float m_Attack;
-    [SerializeField]
-    protected float m_Defense;
-
-    public float Life
+    [System.Serializable]
+    public class PersonaStats : System.Object
     {
-      get { return m_Life; }
-      set { m_Life = value; }
-    }
+        [SerializeField]
+        protected float m_Life;
+        [SerializeField]
+        protected float m_LifeMax;
+        [SerializeField]
+        protected float m_Attack;
+        [SerializeField]
+        protected float m_Defense;
 
-    public float LifeMax
-    {
-      get { return m_LifeMax; }
-      set { m_LifeMax = value; }
+        public float Life
+        {
+            get { return m_Life; }
+            set { m_Life = value; }
+        }
+
+        public float LifeMax
+        {
+            get { return m_LifeMax; }
+            set { m_LifeMax = value; }
+        }
+
+        public float Attack
+        {
+            get { return m_Attack; }
+            set { m_Attack = value; }
+        }
+
+        public float Defense
+        {
+            get { return m_Defense; }
+            set { m_Defense = value; }
+        }
+
     }
-  }
 }
